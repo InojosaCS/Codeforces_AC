dcae253d3ce04f4b79465ec129c52820b5f42eb0
InojosaCS <cristianinojosa@gmail.com>
2020-05-26 15:58:00 -0400

    Add solution for problem `C - Celex Update` Link: https://codeforces.com/contest/1358/problem/C Tags: math, *1600 Ref: https://codeforces.com/contest/1358/submission/81561548

---
+++ codeforces/1358/C.cpp
@@ -0,0 +1,63 @@
+/*
+ID: cristia35
+TASK: cf
+LANG: C++
+*/
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <set>
+#include <stdio.h>	
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<bool> vb;
+typedef vector<ii> vii;
+
+#define solve imAwesome
+#define for0(i,n) for(int i=0; i<(int)n; i++)
+#define forn(i,n) for(int i=1; i<=(int)n; i++)
+#define pb push_back
+#define f first
+#define s second
+#define mp make_pair
+#define ms(obj,val) memset(obj,val,sizeof(obj))
+#define all(x) x.begin(),x.end()
+#define endl cout << "\n";
+#define debug cout << "Aqui\n";
+#define deb(i) cout << "Aqui " << (ll) i << "\n";
+#define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	//ifstream cin ("subset.in");
+	//ofstream cout ("subset.out");
+
+	ll t;
+	cin >> t;
+	while(t--){
+		ll a,b,c,d;
+		cin >> a >> b >> c >>d;
+		cout << (c-a)*(d-b)+1ll << "\n";
+	}
+	
+	return 0;
+
+}
