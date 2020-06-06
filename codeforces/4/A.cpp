1eca17ca284557d773700191d43956fa497c96b2
InojosaCS <cristianinojosa@gmail.com>
2020-06-05 23:44:00 -0400

    Add solution for problem `A - Watermelon` Link: https://codeforces.com/contest/4/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/4/submission/82671681

---
+++ codeforces/4/A.cpp
@@ -0,0 +1,49 @@
+/*
+ * melanie inojosa
+ * */
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<bool> vb;
+typedef vector<ii> vii;
+ 
+#define solve imAwesome
+#define for0(i,n) for(int i=0; i<(int)n; i++)
+#define forn(i,n) for(int i=1; i<=(int)n; i++)
+#define pb push_back
+#define f first
+#define s second
+#define mp make_pair
+#define ms(obj,val) memset(obj,val,sizeof(obj))
+#define all(x) x.begin(),x.end()
+#define sz(x) (ll) x.size()
+#define endl cout << "\n";
+#define debug cout << "Aqui\n";
+#define deb(i) cout << "Aqui " << (ld) i << "\n";
+#define pi 3.141592653589793238462643383279502884197169399375105820974944592307816406286L
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	
+	//auto T=clock();
+	int w;
+	cin >> w;
+	if(w%2==0 and w>2){
+		cout << "YES\n";
+	}else{
+		cout << "NO\n";
+	}
+	
+	
+	
+	
+	//cout<<double(clock()-T)/CLOCKS_PER_SEC<<'\n';
+	return 0;
+ 
+}
