96c4023439f1dabcb45eea3af5c4bb7af9a8a6ce
InojosaCS <cristianinojosa@gmail.com>
2020-07-01 15:58:00 -0400

    Add solution for problem `C - A Cookie for You` Link: https://codeforces.com/contest/1371/problem/C Tags: greedy, implementation, math, *1300 Ref: https://codeforces.com/contest/1371/submission/85716495

---
+++ codeforces/1371/C.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<bool> vb;
+ 
+#define for0(i,n) for(int i=0; i<(int)n; i++)
+#define forn(i,n) for(int i=1; i<=(int)n; i++)
+#define pb push_back
+#define f first
+#define s second
+#define all(x) x.begin(),x.end()
+#define sz(s) (ll) s.size()
+#define endl cout << "\n";
+#define deb(i) cout << "Aqui " << (ld) i << "\n";
+//#define int long long 
+
+
+int main(){
+	//auto T=clock();
+	//cout<<double(clock()-T)/CLOCKS_PER_SEC<<'\n';
+	
+	ios_base::sync_with_stdio(false);	cin.tie(NULL);
+	
+	int t;
+	cin >> t;
+	while(t--){
+		ll a,b,n,m;
+		cin >> a >> b >> n >> m;
+		if(a+b>=m+n && min(a,b)>=m){
+			cout << "yes\n";
+		}else{
+			cout << "no\n";
+		}
+	}
+	return 0;
+ 
+}
