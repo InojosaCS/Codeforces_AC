2a9b2acf9df428eec394aec11b94b01ef08377d9
InojosaCS <cristianinojosa@gmail.com>
2020-07-07 22:52:00 -0400

    Add solution for problem `D - Ehab the Xorcist` Link: https://codeforces.com/contest/1325/problem/D Tags: bitmasks, constructive algorithms, greedy, number theory, *1700 Ref: https://codeforces.com/contest/1325/submission/86239724

--- codeforces/1325/D.cpp
+++ codeforces/1325/D.cpp
@@ -17,10 +17,18 @@ int main(){
 	else
 	{
 		vector<long long> arr(1000,0ll);
+		vector<long long> arr2(1000,0ll);
 		bitset<62> binary(u);
-		arr[0]=u;
+
+		for (long long i = 0; i < 62; ++i)
+			if((1ll << i) & u)
+				arr.push_back(1ll << i);
+		
+		arr2.push_back(u);
 		long long diff = v - u;
 		bitset<62> binaryDiff(diff);
+		sort(all(arr), greater<long long>());
+		sort(all(arr2), greater<long long>());
 
 		for (int i = 0; i < 62; ++i)
 		{
@@ -34,20 +42,39 @@ int main(){
 						cnt++;
 					}
 				}
+				cnt = 0;
+				for (int x = 0; x < sz(arr2) && cnt<2; ++x)
+				{
+					if(!(arr2[x] & (1ll << (i-1)))){
+						arr2[x] += (1ll << (i-1));
+						cnt++;
+					}
+				}
 			}
 		}
 		sort(all(arr), greater<long long>());
-		int n = 0;
+		sort(all(arr2), greater<long long>());
+		int n = 0, m = 0;
 		for (int i = 0; i < sz(arr) && arr[i]!=0; ++i)
 			n++;
+		
+		for (int i = 0; i < sz(arr2) && arr2[i]!=0; ++i)
+			m++;
 
-		cout << n << "\n";
+		cout << min(n,m) << "\n";
 
-		for (int i = 0; i < sz(arr) && arr[i]!=0; ++i)
-			cout << arr[i] << " ";
-	
-		cout << "\n";
-	
+		if(n<m){
+			for (int i = 0; i < sz(arr) && arr[i]!=0; ++i)
+				cout << arr[i] << " ";
+		
+			cout << "\n";
+		}
+		else{
+			for (int i = 0; i < sz(arr2) && arr2[i]!=0; ++i)
+				cout << arr2[i] << " ";
+		
+			cout << "\n";
+		}
 	}
 	return 0;
 }
