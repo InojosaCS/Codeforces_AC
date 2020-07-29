acf6d006ed07837affbf425e75b3f2c42930f98b
InojosaCS <cristianinojosa@gmail.com>
2020-07-28 23:07:00 -0400

    Add solution for problem `B - Obtain Two Zeroes` Link: https://codeforces.com/contest/1260/problem/B Tags: binary search, math, *1300 Ref: https://codeforces.com/contest/1260/submission/88246309

---
+++ codeforces/1260/B.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using ll = long long;
+
+#define all(x) x.begin(), x.end()
+#define sz(x) (int) x.size()
+#define deb(x) cout << (#x) << " is " << (x) << endl;
+#define debv(v) for (int i = 0; i < sz(a); i++) cout << v[i] << " "; cout << "\n";
+
+int32_t main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.precision(20);
+
+	ll n;
+	cin >> n;
+	while(n--){
+		ll a,b;
+		cin >> a >> b;
+		if(2*a>=b && b*2>=a && (a+b)%3==0) cout << "YES\n";
+		else cout << "NO\n";
+	}
+	return 0;
+}
