6e2bf40f44690dc3ea99ceb9a6bfde018611d3e9
InojosaCS <cristianinojosa@gmail.com>
2020-10-25 12:02:00 -0400

    Add solution for problem `B - Modulo Sum` Link: https://codeforces.com/contest/577/problem/B Tags: combinatorics, data structures, dp, two pointers, *1900 Ref: https://codeforces.com/contest/577/submission/96714331

--- codeforces/577/B.cpp
+++ codeforces/577/B.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
-//#pragma GCC optimize("Ofast")
-//#pragma GCC target("avx,avx2,fma")
-//#pragma GCC optimization ("unroll-loops")
-//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 
 using namespace std;
 
