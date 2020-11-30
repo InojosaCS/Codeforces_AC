c4a7d0e7de61432e556c3fa219bb2df6b5f7483d
InojosaCS <cristianinojosa@gmail.com>
2020-11-30 10:39:00 -0400

    Add solution for problem `A - Strange Functions` Link: https://codeforces.com/contest/1455/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1455/submission/99997429

---
+++ codeforces/1455/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+//#pragma GCC optimize("Ofast,unroll-loops")
+//#pragma GCC target("avx,avx2,sse,sse2")
+
+using namespace std; 
+
+#define int int64_t 
+
+void solve() { 
+    string s;
+    cin >> s;
+    cout << s.size() << "\n";
+}
+    
+int32_t main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+    
+	int32_t tt = 1;
+	cin >> tt;
+
+	while(tt-->0) solve();
+	
+	return 0;
+}
